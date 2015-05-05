642957c17b0d54420aa46cf98bbbf4722ffd340b
Félix Leblanc <felix.leblanc1305@gmail.com>
2015-05-05 15:42:51 -0400

    Ajout de validation pour la route /api/users
    Justin et Félix

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,6 +2,15 @@ var User = require('../models/user');
 
 exports.create = function(req, res) {
   User.create(req.body, function(err, created) {
-    res.status(201).json({});
+    if (validateBody(created)) {
+      res.status(201).json({});
+    } else {
+      res.status(400).json({'message': 'Invalid payload'});
+    }
   });
 };
+
+function validateBody(body) {
+  return body.email && body.password && body.type && body.name && body.phone &&
+    body.address && body.birth_date;
+}
--- test/users.js
+++ test/users.js
@@ -5,12 +5,31 @@ var assert = require('assert');
 describe('/api/users', function() {
   describe('POST', function() {
     it('should create a user from the json payload', function(done) {
+      var fake_date = Date.now();
+      var data = {
+        email: 'test@test.com',
+        password: 'test-pass',
+        type: 'test-type',
+        name: 'test-name',
+        phone: '123-123-1234',
+        address: ['test-address', 'test-address2'],
+        birth_date: fake_date
+      };
+      client(app)
+        .post('/api/users')
+        .send(data)
+        .end(function(err, res) {
+          assert.equal(res.status, 201);
+          done();
+        });
+    });
+    it('should return 400 Bad Request if bad payload', function(done) {
       var data = {};
       client(app)
         .post('/api/users')
         .send(data)
         .end(function(err, res) {
-          assert.equal(201, res.status);
+          assert.equal(res.status, 400);
           done();
         });
     });
