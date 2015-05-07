6584c181d197462b6caa31985fb50ec461d40482
Justin Duplessis <drfoliberg@gmail.com>
2015-05-06 20:24:59 -0400

    Fix env bug

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,10 @@
 var extend = require('extend');
 
 var config = require('./config.default');
-var envConfig = require('./config.' + process.env.NODE_ENV) || {};
+
+if (process.env.NODE_ENV) {
+  var envConfig = require('./config.' + process.env.NODE_ENV) || {};
+}
 
 extend(true, config, envConfig);
 
