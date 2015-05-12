d7a302c0675b7adc5df3122d8e094df0d5505931
noryte <noryte@users.noreply.github.com>
2015-05-12 18:44:06 -0400

    Ajout page de connexion et route

--- public/app.js
+++ public/app.js
@@ -2,10 +2,12 @@ angular.module('app', [
   'ngRoute',
   'ui.bootstrap',
   'app.controllers.index',
+  'app.controllers.connexion',
   'app.controllers.navbar',
   'app.controllers.inscrire',
 ]).config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/index.html', controller: 'index'});
     $routeProvider.when('/', {templateUrl: 'partials/inscrire.html', controller: 'inscrire'});
+    $routeProvider.when('/connexion', {templateUrl: 'partials/connexion.html', controller: 'ConnexionCtrl'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
---
+++ public/controllers/connexion.js
@@ -0,0 +1,6 @@
+var connexionControllers = angular.module('app.controllers.connexion', []);
+
+connexionControllers.controller('ConnexionCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+  }]);
+
