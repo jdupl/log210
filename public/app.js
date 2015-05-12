ce8941f748c989666e26e36c55037845b14efb24
noryte <noryte@users.noreply.github.com>
2015-05-12 19:07:06 -0400

    Ajout du lien de connexion et fix pour les routes.

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ angular.module('app', [
   'app.controllers.inscrire',
 ]).config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/index.html', controller: 'index'});
-    $routeProvider.when('/', {templateUrl: 'partials/inscrire.html', controller: 'inscrire'});
+    $routeProvider.when('/inscrire', {templateUrl: 'partials/inscrire.html', controller: 'inscrire'});
     $routeProvider.when('/connexion', {templateUrl: 'partials/connexion.html', controller: 'ConnexionCtrl'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
