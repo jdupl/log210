6afd953f478081db4e6bcb331a391b9c87b3e7dc
noryte <noryte@users.noreply.github.com>
2015-05-14 15:49:31 -0400

    Appel a la api pour la connexion. Fonction de deconnexion. Validation du token. Validation des champs de connexion.

--- public/app.js
+++ public/app.js
@@ -1,11 +1,12 @@
 angular.module('app', [
   'ngRoute',
+  'ngStorage',
   'ui.bootstrap',
   'app.controllers.index',
   'app.controllers.connexion',
   'app.controllers.navbar',
   'app.controllers.inscrire',
-]).config(['$routeProvider', function($routeProvider) {
+]).config(['$routeProvider', '$httpProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/index.html', controller: 'index'});
     $routeProvider.when('/inscrire', {templateUrl: 'partials/inscrire.html', controller: 'inscrire'});
     $routeProvider.when('/connexion', {templateUrl: 'partials/connexion.html', controller: 'ConnexionCtrl'});
