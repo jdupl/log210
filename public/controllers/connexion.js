675e403fc44ab9766ff3a5a5c254397f9a309406
Félix Leblanc <felix.leblanc1305@gmail.com>
2015-05-15 11:26:29 -0400

    Added ngStorage to bower dependencies, reformarted code for the connexion angular controller

--- public/controllers/connexion.js
+++ public/controllers/connexion.js
@@ -9,43 +9,48 @@ angular.module('app.controllers.connexion', [])
     function ($http, $scope, $rootScope, $localStorage, $sessionStorage) {
 
     	$scope.connexion = function() {
+
     		var data = {
-                email: $scope.user.email,
-                password: $scope.user.password
-            }
-    		$http.post('/api/login', data).success(function(data){
-		        if (res.type == false) {
-		            alert(res.data) 
-		            $scope.token = '';   
+          email: $scope.user.email,
+          password: $scope.user.password
+        };
+
+    		$http.post('/api/login', data)
+					.success(function(data){
+		        if (res.type === false) {
+
+	            $scope.token = '';
 		        } else {
-		            $localStorage.token = res.data.token;
-		            $scope.token = $localStorage.token;
-		            window.location = "/";    
+
+	            $localStorage.token = res.data.token;
+	            $scope.token = $localStorage.token;
+	            window.location = "/";
 		        }
-		      	}).error(function(data, status){
-		        
-		                $rootScope.error = 'Failed to signin';
-		                $scope.token = '';
-		      	});
-        };
+	      	})
+					.error(function(data, status){
+            $rootScope.error = 'Failed to signin';
+            $scope.token = '';
+	      	});
+      };
 
-        $scope.tokenValide = function() {
-        	var data = { token: $scope.token}
-            $http.get('api/valider', date).success(function() {
+      $scope.tokenValide = function() {
+      	var data = { token: $scope.token };
+          $http.get('api/valider', date)
+					.success(function() {
 
-            }).error(function(data, status) {
-            	$scope.logout();
-            })
-        };
- 
-        $scope.logout = function() {
-        	delete $localStorage.token;
-        	delete $scope.token;
-            window.location = "/";
-        };
+          }).error(function(data, status) {
+          	$scope.logout();
+          });
+      };
 
-        $scope.token = $localStorage.token;
-        if($scope.token) {
-        	$scope.tokenValide();
-        }
-    }])
+      $scope.logout = function() {
+      	delete $localStorage.token;
+      	delete $scope.token;
+        window.location = "/";
+      };
+
+      $scope.token = $localStorage.token;
+      if($scope.token) {
+      	$scope.tokenValide();
+      }
+    }]);
