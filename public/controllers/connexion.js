9b16c6e51814c8d3196d18cb4762c09d15143e9a
Félix Leblanc <felix.leblanc1305@gmail.com>
2015-05-15 11:41:46 -0400

    Fixed indentation

--- public/controllers/connexion.js
+++ public/controllers/connexion.js
@@ -1,56 +1,56 @@
 angular.module('app.controllers.connexion', [])
 //Controller
 .controller('ConnexionCtrl', [
-	'$http',
+  '$http',
     '$scope',
     '$rootScope',
     '$localStorage',
     '$sessionStorage',
     function ($http, $scope, $rootScope, $localStorage, $sessionStorage) {
 
-    	$scope.connexion = function() {
+      $scope.connexion = function() {
 
-    		var data = {
+        var data = {
           email: $scope.user.email,
           password: $scope.user.password
         };
 
-    		$http.post('/api/login', data)
-					.success(function(data){
-		        if (res.type === false) {
+        $http.post('/api/login', data)
+          .success(function(data){
+            if (res.type === false) {
 
-	            $scope.token = '';
-		        } else {
+              $scope.token = '';
+            } else {
 
-	            $localStorage.token = res.data.token;
-	            $scope.token = $localStorage.token;
-	            window.location = "/";
-		        }
-	      	})
-					.error(function(data, status){
+              $localStorage.token = res.data.token;
+              $scope.token = $localStorage.token;
+              window.location = "/";
+            }
+          })
+          .error(function(data, status){
             $rootScope.error = 'Failed to signin';
             $scope.token = '';
-	      	});
+          });
       };
 
       $scope.tokenValide = function() {
-      	var data = { token: $scope.token };
+        var data = { token: $scope.token };
           $http.get('api/valider', date)
-					.success(function() {
+          .success(function() {
 
           }).error(function(data, status) {
-          	$scope.logout();
+            $scope.logout();
           });
       };
 
       $scope.logout = function() {
-      	delete $localStorage.token;
-      	delete $scope.token;
+        delete $localStorage.token;
+        delete $scope.token;
         window.location = "/";
       };
 
       $scope.token = $localStorage.token;
       if($scope.token) {
-      	$scope.tokenValide();
+        $scope.tokenValide();
       }
     }]);
