9a5329fd28520bed83b4cc31a9e0f204a6b029f0
Robin St-Georges <robin.st-georges@hotmail.com>
2015-05-16 02:58:35 -0400

    Ajout des validations pour inscrire + confirmation de mot de passe(Je doit encore trouver comment setter une valeur de défaut pour le datepicker)

--- public/controllers/inscrire.js
+++ public/controllers/inscrire.js
@@ -1,5 +1,33 @@
 var controllers = angular.module('app.controllers.inscrire', ['ui.date']);
 
 controllers.controller('inscrire', function($scope) {
-
+	
+	$scope.submitForm = function() {
+		// formulaire is submitted at least once
+		$scope.submitted = true;
+		
+		if($scope.inscrireForm.$valid) {
+			alert('form submitted');
+		}
+	}
 });
+
+controllers.directive('passwordCheck', [function () {
+    return {
+        restrict: 'A',
+        scope:true,
+        require: 'ngModel',
+        link: function (scope, elem , attributes,control) {
+            var checker = function () {
+
+                var password1 = scope.$eval(attributes.ngModel); 
+
+                var password2 = scope.$eval(attributes.passwordCheck);
+                return password1 == password2;
+            };
+            scope.$watch(checker, function (n) {
+                control.$setValidity("unique", n);
+            });
+        }
+    };
+}]);
