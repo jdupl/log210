66d60bde6efdaf3984f1e66b469099a92e48bec1
petit-prince <felix.leblanc1305@gmail.com>
2015-05-20 19:39:23 -0400

    Named function to verify the token

--- middleware/login.js
+++ middleware/login.js
@@ -5,21 +5,25 @@ var config = require('../config/config');
 exports.verify = function(req, res, next) {
   var authorizationHeader = req.get('Authorization');
   if (authorizationHeader) {
-    var token = authorizationHeader.split(" ")[1];
-    jwt.verify(token, config.jwt.secret, function(err, decoded) {
-      User.findOne({_id: decoded}).select('-password -__v').exec(function(err, user) {
-        if (user) {
-          req.user = user;
-          next();
-        } else {
-          res.status(401).json({message: 'The user isn\'t registered'});
-        }
-      });
-    });
+    verifyToken(req, res, next, authorizationHeader);
   } else {
     req.user = {
       type: config.types.ANONYMOUS
     };
     next();
   }
+};
+
+verifyToken = function(req, res, next, authorizationHeader) {
+  var token = authorizationHeader.split(" ")[1];
+  jwt.verify(token, config.jwt.secret, function(err, decoded) {
+    User.findOne({_id: decoded}).select('-password -__v').exec(function(err, user) {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(401).json({message: 'The user isn\'t registered'});
+      }
+    });
+  });
 };
