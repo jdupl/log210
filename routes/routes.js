faa5457a86c98f8361599e325d59dee67e51b309
Justin Duplessis <drfoliberg@gmail.com>
2015-05-25 19:27:49 -0400

    Prepare routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,11 +4,18 @@ var loginController = require('../controllers/login');
 var restaurantsController = require('../controllers/restaurants');
 
 module.exports = function(app) {
-  app.post('/api/users/', loginMiddleware.verify, usersController.create);
   app.post('/api/login/', loginController.getToken);
+  app.get('/api/profile/', loginMiddleware.verify, usersController.getProfile);
+
+  app.post('/api/users/', loginMiddleware.verify, usersController.create);
+  // /api/users/?type=restaurateur
   app.get('/api/users/', loginMiddleware.verify, usersController.getUsers);
   app.put('/api/users/:id', loginMiddleware.verify, usersController.updateUser);
-  app.post('/api/restaurants', loginMiddleware.verify, restaurantsController.createRestaurant);
+  app.get('/api/users/:id/restaurants/', loginMiddleware.verify, usersController.getRestaurants);
+
+  app.get('/api/restaurants/', loginMiddleware.verify, restaurantsController.getRestaurants);
+  app.get('/api/restaurants/:id', loginMiddleware.verify, restaurantsController.getRestaurant);
+  app.post('/api/restaurants/', loginMiddleware.verify, restaurantsController.createRestaurant);
   app.delete('/api/restaurants/:id', loginMiddleware.verify, restaurantsController.deleteRestaurant);
   app.put('/api/restaurants/:id', loginMiddleware.verify, restaurantsController.updateRestaurant);
 };
