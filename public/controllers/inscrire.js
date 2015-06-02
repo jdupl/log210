414d1336ddf9c00119c000a007069862a4bb3b5e
Justin Duplessis <drfoliberg@gmail.com>
2015-06-01 20:57:37 -0400

    Ajout form de base sans validation client, mais avec gestion des erreurs

--- public/controllers/inscrire.js
+++ public/controllers/inscrire.js
@@ -18,22 +18,3 @@ controllers.controller('Inscrire', function($scope, $http) {
     }
   };
 });
-
-controllers.directive('passwordCheck', [function () {
-  return {
-    restrict: 'A',
-    scope: true,
-    require: 'ngModel',
-    link: function (scope, elem , attributes,control) {
-      var checker = function () {
-        var password1 = scope.$eval(attributes.ngModel);
-        var password2 = scope.$eval(attributes.passwordCheck);
-
-        return password1 == password2;
-      };
-      scope.$watch(checker, function (n) {
-        control.$setValidity("unique", n);
-      });
-    }
-  };
-}]);
--- public/controllers/modifier_restaurateurs.js
+++ public/controllers/modifier_restaurateurs.js
@@ -1,5 +1,28 @@
 var controllers = angular.module('app.controllers.ModifierRestaurateurs', []);
 
-controllers.controller('ModifierRestaurateurs', function($scope, Auth) {
-  
+controllers.controller('ModifierRestaurateurs', function($scope, $http) {
+
+  $scope.alerts = [];
+
+  $scope.submitRestaurateur = function() {
+    $scope.alerts = [];
+    var user = $scope.user;
+
+    if (!user.restaurants) {
+      $scope.alerts.push({msg: "Attention: Le restaurateur n'a pas de restaurant associé !", type: 'warning'});
+    }
+
+    $http.post('/api/users', user)
+      .success(function(data) {
+        delete $scope.user;
+        $scope.alerts.push({msg: "Le restaurateur a été ajouté.", type: 'success'});
+      })
+      .error(function(data, status) {
+        $scope.alerts.push({msg: "Malheuresement, une erreur est survenue lors de l'inscription et l'utilisateur n'a pas pu être enregistré.", type: 'danger'});
+      });
+  };
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
 });
