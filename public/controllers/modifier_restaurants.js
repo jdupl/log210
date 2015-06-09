cafbd9dc19c3d57a280a1deaea812d4c35895f17
Robin St-Georges <robin.st-georges@hotmail.com>
2015-06-08 20:42:46 -0400

    modifier_restaurants.js et modifier_restaurants.html should now be completed(list is still missing)

--- public/controllers/modifier_restaurants.js
+++ public/controllers/modifier_restaurants.js
@@ -1,5 +1,40 @@
 var controllers = angular.module('app.controllers.ModifierRestaurants', []);
 
-controllers.controller('ModifierRestaurants', function($scope, Auth) {
-
+controllers.controller('ModifierRestaurants', function($scope, $http) {
+  $scope.alerts = [];
+  refreshlist();
+  
+  $scope.submitRestaurant = function() {
+    $scope.alerts = [];
+	var restaurant = $scope.restaurant;
+	
+	if(!restaurant.restaurateur) {
+	  $scope.alerts.push({msg: "Attention: Le restaurant n'a pas de restaurateur associé !", type: 'warning'});
+	}
+	
+	$http.post('/api/restaurants', restaurant, {headers: {'Authorization' : 'Bearer ' + $scope.token}})
+      .success(function(data) {
+        delete $scope.restaurant;
+        $scope.alerts.push({msg: "Le restaurant a été ajouté.", type: 'success'});
+        refreshList();
+      })
+      .error(function(data, status) {
+        $scope.alerts.push({msg: "Malheuresement, une erreur est survenue lors de l'inscription et le restaurant n'a pas pu être enregistré.", type: 'danger'});
+      });
+  };
+  
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
+  
+  function refreshlist() {
+    $http.get('/api/restaurants', {headers: {'Authorization' : 'Bearer ' + $scope.token}})
+      .success(function(data) {
+        $scope.restaurants = data;
+        if ($scope.restaurants.length === 0) {
+          $scope.noRestaurants = true;
+        }
+      });
+  }
+  
 });
