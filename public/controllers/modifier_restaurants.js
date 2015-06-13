a6cf5562872abe5eb4e9577a1bba4619fa56c93f
Justin Duplessis <drfoliberg@gmail.com>
2015-06-13 14:46:09 -0400

    fix indent

--- public/controllers/modifier_restaurants.js
+++ public/controllers/modifier_restaurants.js
@@ -3,16 +3,16 @@ var controllers = angular.module('app.controllers.ModifierRestaurants', []);
 controllers.controller('ModifierRestaurants', function($scope, $http) {
   $scope.alerts = [];
   refreshList();
-  
+
   $scope.submitRestaurant = function() {
     $scope.alerts = [];
-	var restaurant = $scope.restaurant;
-	
-	if(!restaurant.restaurateur) {
-	  $scope.alerts.push({msg: "Attention: Le restaurant n'a pas de restaurateur associé !", type: 'warning'});
-	}
-	
-	$http.post('/api/restaurants', restaurant, {headers: {'Authorization' : 'Bearer ' + $scope.token}})
+    var restaurant = $scope.restaurant;
+
+    if(!restaurant.restaurateur) {
+      $scope.alerts.push({msg: "Attention: Le restaurant n'a pas de restaurateur associé !", type: 'warning'});
+    }
+
+    $http.post('/api/restaurants', restaurant, {headers: {'Authorization' : 'Bearer ' + $scope.token}})
       .success(function(data) {
         delete $scope.restaurant;
         $scope.alerts.push({msg: "Le restaurant a été ajouté.", type: 'success'});
@@ -22,32 +22,30 @@ controllers.controller('ModifierRestaurants', function($scope, $http) {
         $scope.alerts.push({msg: "Malheuresement, une erreur est survenue lors de l'inscription et le restaurant n'a pas pu être enregistré.", type: 'danger'});
       });
   };
-  
+
   $scope.closeAlert = function(index) {
     $scope.alerts.splice(index, 1);
   };
-  
+
   $scope.changeRestaurantInfo = function() {
-	$scope.showModifyForm = true;
-	
-	$http.get('/api/restaurants/:id', {headers: {'Authorization' : 'Bearer ' + $scope.token}})
+    $scope.showModifyForm = true;
+
+    $http.get('/api/restaurants/:id', {headers: {'Authorization' : 'Bearer ' + $scope.token}})
       .success(function(data) {
-		$scope.restaurant = data;
-		console.log(JSON.stringify(data));
+        $scope.restaurant = data;
+        console.log(data);
+        $scope.alerts.push({msg: "Vous avez commmencé la modification !", type: 'warning'});
       });
-	
-    $scope.alerts.push({msg: "Vous avez commmencé la modification !", type: 'warning'});
   };
-  
+
   $scope.deleteRestaurant = function() {
-	
-	$http.delete('/api/restaurants/:id', {headers: {'Authorization' : 'Bearer ' + $scope.token}})
+    $http.delete('/api/restaurants/:id', {headers: {'Authorization' : 'Bearer ' + $scope.token}})
       .success(function(data) {
-		$scope.alerts.push({msg: "Restaurant détruit!", type: 'warning'});
+        $scope.alerts.push({msg: "Restaurant détruit!", type: 'warning'});
+        refreshList();
       });
-	  refreshList();
   };
-  
+
   function refreshList() {
     $http.get('/api/restaurants', {headers: {'Authorization' : 'Bearer ' + $scope.token}})
       .success(function(data) {
@@ -57,5 +55,5 @@ controllers.controller('ModifierRestaurants', function($scope, $http) {
         }
       });
   }
-  
+
 });
