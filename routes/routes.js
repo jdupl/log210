0e3d850a030390cee8808df2b6f0fb3d8da479c5
petit-prince <felix.leblanc1305@gmail.com>
2015-06-13 15:06:10 -0400

    DELETE /api/users/:id route to delete a user

--- routes/routes.js
+++ routes/routes.js
@@ -8,14 +8,18 @@ var loginController = require('../controllers/login');
 var restaurantsController = require('../controllers/restaurants');
 
 module.exports = function(app) {
-  app.post('/api/users/', loginMiddleware.verify, userPayloadValidation, usersController.create);
+  //Login routes
   app.post('/api/login/', loginController.getToken);
   app.get('/api/profile/', loginMiddleware.verify, loginController.getProfile);
 
+  //Users routes
+  app.post('/api/users/', loginMiddleware.verify, userPayloadValidation, usersController.create);
   app.get('/api/users/', loginMiddleware.verify, usersController.getUsers);
   app.put('/api/users/:id', loginMiddleware.verify, userPayloadValidation, usersController.updateUser);
+  app.delete('/api/users/:id', loginMiddleware.verify, usersController.deleteUser);
   app.get('/api/users/:id/restaurants/', loginMiddleware.verify, usersController.getRestaurants);
 
+  //Restaurants routes
   app.get('/api/restaurants/', loginMiddleware.verify, restaurantsController.getRestaurants);
   app.get('/api/restaurants/:id', loginMiddleware.verify, restaurantsController.getRestaurant);
   app.post('/api/restaurants/', loginMiddleware.verify, restaurantsController.createRestaurant);
