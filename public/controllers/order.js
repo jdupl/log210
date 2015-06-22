dab69e1b168d387506a19d803c272ff6902b7b4b
Robin St-Georges <robin.st-georges@hotmail.com>
2015-06-22 19:08:48 -0400

    merge preps

--- public/controllers/order.js
+++ public/controllers/order.js
@@ -1,7 +1,6 @@
 var controllers = angular.module('app.controllers.Order', ['ui.date']);
 
 controllers.controller('Order', function($scope, $http) {
-  <!-- functions -->
 
   $scope.submitOrder = function() {
 
