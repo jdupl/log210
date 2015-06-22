d9f658f9d92a8e22e60991afec9c10cd1007e5ff
Justin Duplessis <drfoliberg@gmail.com>
2015-06-22 19:06:18 -0400

    Fix order ctrl

--- public/controllers/order.js
+++ public/controllers/order.js
@@ -1,9 +1,5 @@
 var controllers = angular.module('app.controllers.Order', ['ui.date']);
 
 controllers.controller('Order', function($scope, $http) {
-  <!-- functions -->
 
-  $scope.submitOrder = function() {
-
-  };
 });
