527aaf4f60995a37040d1cbeda88f7e8a36e45f5
Félix Leblanc <felix.leblanc1305@gmail.com>
2015-06-24 12:15:03 -0400

    GET /api/profile/orders Get all the orders from the logged in user

--- test/routes/orders.js
+++ test/routes/orders.js
@@ -7,6 +7,7 @@ var Order = require('../../models/order');
 
 var app = require('../../app');
 var client = require('supertest')(app);
+var config = require('../../config/config');
 
 var data = require('../utils/data');
 var login = require('../utils/login');
@@ -44,6 +45,56 @@ describe('/api/orders', function() {
                       done();
                     });
                   });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
+describe('/api/profile/orders', function() {
+  describe('GET', function() {
+    it('should get all the orders from the logged in user', function(done) {
+      User.create(data.client_user, function(err, createdClient) {
+
+        Plate.create(data.test_plate, function(err, createdPlate) {
+          Restaurant.create(data.test_restaurant, function(err, createdRestaurant) {
+            var restaurants = [createdRestaurant._id];
+            var test_restaurateur = extend(test_restaurateur, data.restaurateur_user);
+            test_restaurateur.restaurants = restaurants;
+
+            User.create(test_restaurateur, function(err, createdRestaurateur) {
+              plates = [createdPlate._id];
+              var test_item = extend(test_item, data.test_item);
+
+              Item.create(test_item, function(err, createdItem) {
+                var items = [createdItem._id];
+                var test_order = extend(test_order, data.test_order);
+                test_order.client = createdClient._id;
+                test_order.status = config.status.ORDERED;
+                test_order.restaurant = createdRestaurant._id;
+                test_order.items = items;
+
+                Order.create(test_order, function(err, createdOrder) {
+
+                  login.getToken(test_restaurateur.email, test_restaurateur.password, client, function(err, token) {
+                    client
+                      .get('/api/profile/orders')
+                      .set('Authorization', 'Bearer ' + token)
+                      .end(function(err, res) {
+                        assert.equal(res.status, 200);
+                        assert.equal(res.body.length, 1);
+                        order = res.body[0];
+                        assert.equal(order.client, createdClient._id);
+                        assert.equal(order.status, config.status.ORDERED);
+                        assert.equal(order.restaurant, createdRestaurant._id);
+                        assert.equal(order.items.length, 1);
+                        assert.equal(order.items[0], createdItem._id);
+                        done();
+                      });
+                  });
+                });
               });
             });
           });
