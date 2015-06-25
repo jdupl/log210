03600588ee558c55cefdc4138ce5144a50030815
noryte <noryte@users.noreply.github.com>
2015-06-25 17:42:18 -0400

    Utilisation de ng-model

--- public/controllers/menu_restaurant.js
+++ public/controllers/menu_restaurant.js
@@ -3,31 +3,16 @@ var controllers = angular.module('app.controllers.MenuRestaurant', []);
 controllers.controller('MenuRestaurant', function($scope, $http, $routeParams, Auth) {
 
   $scope.submitOrder = function () {
-  var elements = document.getElementById("order").elements;
-  var menu = {};
-  for (var i = 0, element; element = elements[i++];) {
-      if (element.type === "number" && element.value !== "" && element.value > 0) {
-        var res = element.id.split(":");
-        var plate = '{ "' + $scope.menus[res[0]].plates[res[1]]._id + '" : ' + element.value + ' }';
-        if(!menu[$scope.menus[res[0]]._id]) {
-          menu[$scope.menus[res[0]]._id] = [];
-        }
-        menu[$scope.menus[res[0]]._id].push(plate);
-      }
-    }
-  var data = {};
-  data["restaurant"] = $routeParams.restaurantId;
-  data["commande"] = menu;
-  JSON.stringify(data);
+    // Goto order confirmation
   }
 
   $scope.getRestaurant =  function(id) {
     $http.get('/api/restaurants/' + id + '/menus', {headers: {'Authorization' : 'Bearer ' + $scope.token}})
       .success(function(data) {
-        $scope.menus = data;
+        $scope.order = data;
       });
   }
-  
+
   $scope.token = Auth.isLoggedIn();
   $scope.getRestaurant($routeParams.restaurantId);
 });
