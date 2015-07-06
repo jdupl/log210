6fb907658da07639cb2ec682705987eae64707fe
Félix Leblanc <felix.leblanc1305@gmail.com>
2015-07-05 21:15:32 -0400

    Fix on config.test.js to return a transporter stub

--- config/config.test.js
+++ config/config.test.js
@@ -1,8 +1,16 @@
-module.exports = {
+var config = {
   db: {
     url: 'mongodb://localhost/test'
   },
   jwt: {
     secret: 'test'
   }
-};
+}
+
+var stubTransporter = {
+  sendMail: function(mailOptions, callback) { callback(null, null); }
+}
+
+config.transporter = stubTransporter;
+
+module.exports = config;
