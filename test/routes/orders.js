a8878183dd6d43f73cd6d5563c6ba5e4967e8e58
Félix Leblanc <felix.leblanc1305@gmail.com>
2015-07-14 17:24:13 -0400

    GET /api/orders?status=3 Get all the orders with the ready status number

--- test/routes/orders.js
+++ test/routes/orders.js
@@ -200,3 +200,54 @@ describe('/api/orders/:id', function() {
     });
   });
 });
+describe('/api/orders/', function() {
+  describe('GET', function() {
+    it('should get the orders with the ready status', function(done) {
+      User.create(data.client_user, function(err, createdClient) {
+
+        Plate.create(data.test_plate, function(err, createdPlate) {
+          Restaurant.create(data.test_restaurant, function(err, createdRestaurant) {
+            plates = [createdPlate._id];
+            var test_item = extend(test_item, data.test_item);
+
+            Item.create(test_item, function(err, createdItem) {
+              var items = [createdItem._id];
+              var test_order = extend(test_order, data.test_order);
+              test_order.client = createdClient._id;
+              test_order.status = config.status.READY;
+              test_order.restaurant = createdRestaurant._id;
+              test_order.items = items;
+
+              Order.create(test_order, function(err, createdOrder) {
+
+                var test_order_not_ready = {
+                  status: config.status.ORDERED
+                };
+
+                Order.create(test_order_not_ready, function(err, createdOrderNotReady) {
+
+                  login.getToken(data.client_user.email, data.client_user.password, client, function(err, token) {
+                    client
+                    .get('/api/orders?status=3')
+                    .set('Authorization', 'Bearer ' + token)
+                    .end(function(err, res) {
+                      assert.equal(res.status, 200);
+                      assert.equal(res.body.length, 1);
+                      order = res.body[0];
+                      assert.equal(order.client, createdClient._id);
+                      assert.equal(order.status, config.status.READY);
+                      assert.equal(order.restaurant, createdRestaurant._id);
+                      assert.equal(order.items.length, 1);
+                      assert.equal(order.items[0], createdItem._id);
+                      done();
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
